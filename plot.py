07d8d89304188035b749509d2810960ee9658af6
shreyas03varude <130708156+shreyas03varude@users.noreply.github.com>
2025-10-16 15:53:11 -0500

    Update plot.py

--- plot.py
+++ plot.py
@@ -4,28 +4,37 @@ import matplotlib.pyplot as plt
 import sys
 
 def main():
-    L = sys.argv[1]
-    for ratio in np.linspace(0, 1.0, 11, dtype=float):
-        datap = pd.read_csv("datas/" + str(L) + "/L=" + str(L) + " and ratio = " + str(ratio)[:3] + ".csv", header=None).values
-        datap = datap[:,:-1]
-        datap = np.concatenate((datap[10:][::-1], datap), axis=0)
-        fig, ax = plt.subplots(figsize=(10, 8))
-        im = ax.imshow(np.transpose(datap[:, ::-1]), 
-                    interpolation='none', 
-                    aspect='auto')  # plots with columns reversed
-        xticks = np.linspace(0, datap.shape[0] - 1, 5)
-        xlabels = [r'$0$',r'$\frac{\pi}{2}$',r'$\pi$',r'$\frac{3\pi}{2}$',r'$2\pi$']
-        yticks = np.linspace(datap.shape[1] - 1, 0, 7)
-        ylabels = np.linspace(0, 3, 7)
-        ax.set_xticks(ticks=xticks, labels=xlabels)
-        ax.set_yticks(ticks=yticks, labels=ylabels)
-        ax.set_title('L = ' + str(L) + ', J2/J1 = ' + str(ratio)[:3])
-        ax.set_xlabel('q')
-        ax.set_ylabel('$\omega$')
-        # ax.xaxis.set_major_formatter(tck.FormatStrFormatter('%g $\pi$'))
-        # ax.xaxis.set_major_locator(tck.MultipleLocator(base=0.5))
-        fig.colorbar(im, ax=ax, shrink=0.5)
-        plt.savefig("plots/" + str(L) + "/plot for L = " + str(L) + " and ratio = " + str(ratio)[:3] + ".png")
 
-if __name__ == "__main__":
+    if len(sys.argv) != 6:
+        print('Usage:\n\tpython plot.py <L> <delta> <J1> <J2> <input filename> <output filename>')
+        quit()
+    L = str(sys.argv[1])
+    delta = sys.argv[2]
+    J1 = str(sys.argv[3])
+    J2 = str(sys.argv[4])
+    input = sys.argv[5]
+    output = sys.argv[6]
+
+    datap = pd.read_csv(input)
+    datap = datap.iloc[:,:-1]
+    datap = np.concatenate((datap[10:][::-1], datap), axis=0)
+
+    fig, ax = plt.subplots(figsize=(10, 8))
+    im = ax.imshow(np.transpose(datap[:, ::-1]),        
+                interpolation='none',
+                aspect='auto')  # plots with columns reversed
+
+    xticks = np.linspace(0, datap.shape[0] - 1, 5)      
+    xlabels = [r'$0$',r'$\frac{\pi}{2}$',r'$\pi$',r'$\frac{3\pi}{2}$',r'$2\pi$']
+    yticks = np.linspace(datap.shape[1] - 1, 0, 7)      
+    ylabels = np.linspace(0, 3, 7)
+    ax.set_xticks(ticks=xticks, labels=xlabels)
+    ax.set_yticks(ticks=yticks, labels=ylabels)
+    ax.set_title('L = ' + L + ', J1 = ' + J1 + ', J2 = ' + J2 + ', $\\delta$ = ' + delta)
+    ax.set_xlabel('q')
+    ax.set_ylabel('$\omega$')
+    fig.colorbar(im, ax=ax, shrink=0.5)
+    plt.savefig(output)
+
+if __name__ == '__main__':
    main()
